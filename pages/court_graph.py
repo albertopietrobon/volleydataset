a6508267060022fd3f3b907c0f5d7903e9204c5f
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-05-14 13:30:39 +0000

    c16

--- pages/court_graph.py
+++ pages/court_graph.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 if "info_type" not in st.session_state:
     st.session_state.info_type = "errors"
@@ -103,5 +104,121 @@ if st.session_state.fundamental_type == "receive":
     if st.session_state.info_type == "errors":
         focus_receive = focus_receive[(focus_receive['score'] == 'L') & (focus_receive['point_type'] == 'opp point') & (focus_receive['serve_zone'].notna())]
         st.dataframe(focus_receive)
-    
 
+#crea colonne con numero delle zone start/end dell'azione  
+att = pd.DataFrame({
+    'start_att' : focus_att['attack_zone'].str.extract(r'att_(\d+)')[0].dropna().astype(int),
+    'end_att' : focus_att['defense_zone'].str.extract(r'def_(\d+)')[0].dropna().astype(int)
+
+})
+att = att.reset_index(drop=True)
+
+#crea vettore con frequenza zone di attacco
+frequenza_attacchi = att['start_att'].value_counts(normalize=True).sort_index().reindex(range(1, 7), fill_value=0)
+frequenza_difese = att['end_att'].value_counts(normalize=True).sort_index().reindex(range(1, 11), fill_value=0)
+
+
+
+
+
+
+def plot_volleyball_attack_frequency(attack_frequencies,defense_frequencies, player_name="Giocatore"):
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.axis('off')
+    ax.set_aspect('equal')
+    plt.title(f'{player_name} attack distribution', fontsize=14)
+
+    # Dimensioni del campo (proporzionali)
+    court_width = 9
+    court_length = 18
+
+    # Disegna il campo
+    rect = plt.Rectangle((0,0), court_width, court_length, edgecolor='black',linewidth=3, facecolor='lightgrey', alpha=1)
+    ax.add_patch(rect)
+    plt.plot([court_width/3,court_width/3], [0, court_length], 'k--', linewidth=1) #3m verticale
+    plt.plot([2*court_width /3, 2*court_width /3], [0, court_length], 'k--', linewidth=1) #6m verticale
+    plt.plot([0,court_width], [court_length/3,court_length/3], 'k-', linewidth=2) # Linea dei 3m nostra
+    plt.plot([0,court_width], [2*court_length/3,2*court_length/3], 'k-', linewidth=2)  # Linea dei 3m avversaria
+    plt.plot([0,court_width], [5*court_length/6,5*court_length/6], 'k--', linewidth=1)  # Linea dei 6m avversaria
+    plt.plot([0,court_width], [court_length/2,court_length/2], 'k-', linewidth=3)
+
+    # Coordinate delle zone di attacco 
+    zone_coords_att = {
+       1: (2*court_width/3,0), 
+       2: (2*court_width/3,court_length/3),
+       3: (court_width/3,court_length/3),
+       4: (0,court_length/3), 
+       5: (0,0), 
+       6: (court_width/3,0)
+    }
+
+
+    cmap1 = plt.cm.summer  # Scegli la colormap che preferisci
+    max_freq1 = attack_frequencies.max() if not attack_frequencies.empty else 1
+
+    norm = plt.Normalize(vmin=0, vmax=max_freq1)
+    sm1 = plt.cm.ScalarMappable(cmap=cmap1.reversed(), norm=norm)
+    sm1.set_array([])    
+
+    for zone, freq in attack_frequencies.items():
+        x, y = zone_coords_att[zone]
+        color = cmap1(1-freq / max_freq1) if max_freq1 > 0 else 'lightgray'
+        if 2 <= zone <= 4:
+            circle = plt.Rectangle((x, y),height=3,width=3, facecolor=color, edgecolor=None, alpha=1)
+            ax.add_patch(circle)
+            ax.text(x+1.5, y+1.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+        else:
+            circle = plt.Rectangle((x, y),height=6,width=3, facecolor=color, edgecolor=None, alpha=1)
+            ax.add_patch(circle)
+            ax.text(x+1.5, y+3, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+
+    
+    cbar1 = plt.colorbar(sm1, ax=ax, orientation='vertical', pad=0.1)
+    cbar1.set_label('Frequenza Relativa')
+    cbar1.ax.tick_params(labelsize=8)
+
+    # Coordinate delle zone di difesa 
+    zone_coords_def = {
+       1: (0,court_length), 
+       2: (0,2*court_length/3),
+       3: (court_width/3,2*court_length/3),
+       4: (2*court_width/3,2*court_length/3), 
+       5: (2*court_width/3,court_length), 
+       6: (court_width/3,court_length),
+       8: (0,5*court_length/6),
+       9: (2*court_width/3,5*court_length/6),
+       10:(court_width/3,5*court_length/6)
+    }
+
+    cmap2 = plt.cm.spring  # Scegli la colormap che preferisci
+    max_freq2 = defense_frequencies.max() if not defense_frequencies.empty else 1
+
+    norm = plt.Normalize(vmin=0, vmax=max_freq2)
+    sm2 = plt.cm.ScalarMappable(cmap=cmap2.reversed(), norm=norm)
+    sm2.set_array([])    
+
+    for zone, freq in defense_frequencies.items():
+    
+        if 1<= zone <= 6:
+            x, y = zone_coords_def[zone]
+            color = cmap2(1-freq / max_freq2) if max_freq2 > 0 else 'lightgray'
+            circle = plt.Rectangle((x, y),height=-3,width=3, facecolor=color, edgecolor=None, alpha=1)
+            ax.add_patch(circle)
+            ax.text(x+1.5, y-1.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+        if 8<= zone <= 10:
+            x, y = zone_coords_def[zone]
+            color = cmap2(1-freq / max_freq2) if max_freq2 > 0 else 'lightgray'
+            circle = plt.Rectangle((x, y),height=-3,width=3, facecolor=color, edgecolor=None, alpha=1)
+            ax.add_patch(circle)
+            ax.text(x+1.5, y-1.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+        
+    
+    cbar2 = plt.colorbar(sm2, ax=ax, orientation='vertical', pad=0.1)
+    cbar2.set_label('Frequenza Relativa')
+    cbar2.ax.tick_params(labelsize=8)
+
+    st.pyplot(fig) 
+
+# Esegui la funzione per visualizzare il grafico
+plot_volleyball_attack_frequency(frequenza_attacchi,frequenza_difese, st.session_state.player)
