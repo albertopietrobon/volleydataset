c616307ac80fbf945b869bda350d5b940c4786d4
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-05-15 14:09:57 +0000

    c22

--- pages/court_graph.py
+++ pages/court_graph.py
@@ -14,13 +14,6 @@ if "player" not in st.session_state:
     st.session_state.player = "Paola Egonu"
 
 
-
-
-
-
-
-
-
 def plot_volleyball_attack_frequency(attack_frequencies,defense_frequencies,transizioni_frequenze, player_name="Giocatore",soglia_freq=0.01):
     
     if st.session_state.info_type == "points":
@@ -325,12 +318,6 @@ def plot_volleyball_attack_frequency(attack_frequencies,defense_frequencies,tran
                         )
                         ax.add_patch(arrow)
         st.pyplot(fig) 
-
-    
-    
-
-
-
 def plot_volleyball_serve_frequency(attack_frequencies,defense_frequencies,transizioni_frequenze, player_name="Giocatore",soglia_freq=0.01):
     
     if st.session_state.info_type == "points":
@@ -637,11 +624,6 @@ def plot_volleyball_serve_frequency(attack_frequencies,defense_frequencies,trans
                         )
                         ax.add_patch(arrow)
         st.pyplot(fig) 
-
-
-
-
-
 def plot_volleyball_block_frequency(attack_frequencies, player_name="Giocatore"):
     
     
@@ -696,12 +678,6 @@ def plot_volleyball_block_frequency(attack_frequencies, player_name="Giocatore")
     cbar1.ax.tick_params(labelsize=8)
 
     st.pyplot(fig) 
-
-
-
-
-
-
 def plot_volleyball_defense_frequency(attack_frequencies,defense_frequencies,transizioni_frequenze, player_name="Giocatore",soglia_freq=0.01):
     
     if st.session_state.info_type == "errors":
@@ -851,47 +827,158 @@ def plot_volleyball_defense_frequency(attack_frequencies,defense_frequencies,tra
                         )
                         ax.add_patch(arrow)
         st.pyplot(fig) 
+def plot_volleyball_receive_frequency(attack_frequencies,defense_frequencies,transizioni_frequenze, player_name="Giocatore",soglia_freq=0.01):
     
     
+    if st.session_state.info_type == "errors":
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.axis('off')
+        ax.set_aspect('equal')
+        plt.title(f'{player_name} receive distribution', fontsize=14)
 
-    
-    
+        # Dimensioni del campo (proporzionali)
+        court_width = 9
+        court_length = 18
 
+        # Disegna il campo
+        rect = plt.Rectangle((0,0), court_width, court_length, edgecolor='black',linewidth=3, facecolor='lightgrey', alpha=1)
+        ax.add_patch(rect)
+        plt.plot([court_width/3,court_width/3], [court_length, court_length+1], 'k--', linewidth=1) #3m verticale
+        plt.plot([2*court_width /3, 2*court_width /3], [court_length, court_length+1], 'k--', linewidth=1) #6m verticale
+        plt.plot([court_width/3,court_width/3], [0,court_length/2], 'k--', linewidth=1) #3m verticale
+        plt.plot([2*court_width /3, 2*court_width /3], [0,court_length/2], 'k--', linewidth=1) #6m verticale
+        plt.plot([0,0], [court_length, court_length+1], 'k--', linewidth=1) #3m verticale
+        plt.plot([0,court_width], [court_length+1,court_length+1], 'k--', linewidth=1) #6m verticale
+        plt.plot([court_width ,court_width], [court_length, court_length+1], 'k--', linewidth=1)
+        plt.plot([0,court_width], [court_length/3,court_length/3], 'k-', linewidth=2) # Linea dei 3m nostra
+        plt.plot([0,court_width], [2*court_length/3,2*court_length/3], 'k-', linewidth=2)  # Linea dei 3m avversaria
+        plt.plot([0,court_width], [court_length/6,court_length/6], 'k--', linewidth=1)  # Linea dei 6m avversaria
+        plt.plot([0,court_width], [court_length/2,court_length/2], 'k-', linewidth=3)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Coordinate delle zone di attacco 
+        zone_coords_att = {
+            1: (0,court_length+1), 
+            5: (2*court_width/3,court_length+1), 
+            6: (court_width/3,court_length+1)
+        }
 
 
+        cmap1 = plt.cm.autumn  # Scegli la colormap che preferisci
+        max_freq1 = attack_frequencies.max() if not attack_frequencies.empty else 1
+
+        norm = plt.Normalize(vmin=0, vmax=max_freq1)
+        sm1 = plt.cm.ScalarMappable(cmap=cmap1.reversed(), norm=norm)
+        sm1.set_array([])    
+
+        for zone, freq in attack_frequencies.items():
+
+            if 5<= zone <= 6:
+                x, y = zone_coords_att[zone]
+                color = cmap1(1-freq / max_freq1) if max_freq1 > 0 else 'lightgray'
+                circle = plt.Rectangle((x, y),height=-1,width=3, facecolor=color, edgecolor=None, alpha=1)
+                ax.add_patch(circle)
+                ax.text(x+1.5, y-0.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+            if zone == 1:
+                x, y = zone_coords_att[zone]
+                color = cmap1(1-freq / max_freq1) if max_freq1 > 0 else 'lightgray'
+                circle = plt.Rectangle((x, y),height=-1,width=3, facecolor=color, edgecolor=None, alpha=1)
+                ax.add_patch(circle)
+                ax.text(x+1.5, y-0.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+
+        
+        cbar1 = plt.colorbar(sm1, ax=ax, orientation='vertical', pad=-0.5, location='right')
+        cbar1.set_label('Serving zone [%]')
+        cbar1.ax.tick_params(labelsize=8)
+
+        # Coordinate delle zone di difesa 
+        zone_coords_def = {
+            1: (2*court_width/3,0), 
+            2: (2*court_width/3,court_length/3),
+            3: (court_width/3,court_length/3),
+            4: (0,court_length/3), 
+            5: (0,0), 
+            6: (court_width/3,0),
+            8: (2*court_width/3,court_length/6),
+            9: (0,court_length/6),
+            10:(court_width/3,court_length/6)
+        }
+
+        cmap2 = plt.cm.autumn  # Scegli la colormap che preferisci
+        max_freq2 = defense_frequencies.max() if not defense_frequencies.empty else 1
+
+        norm = plt.Normalize(vmin=0, vmax=max_freq2)
+        sm2 = plt.cm.ScalarMappable(cmap=cmap2.reversed(), norm=norm)
+        sm2.set_array([])    
+
+        for zone, freq in defense_frequencies.items():
+        
+            if 1<= zone <= 6:
+                x, y = zone_coords_def[zone]
+                color = cmap2(1-freq / max_freq2) if max_freq2 > 0 else 'lightgray'
+                circle = plt.Rectangle((x, y),height=3,width=3, facecolor=color, edgecolor=None, alpha=1)
+                ax.add_patch(circle)
+                ax.text(x+1.5, y+1.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+            if 8<= zone <= 10:
+                x, y = zone_coords_def[zone]
+                color = cmap2(1-freq / max_freq2) if max_freq2 > 0 else 'lightgray'
+                circle = plt.Rectangle((x, y),height=3,width=3, facecolor=color, edgecolor=None, alpha=1)
+                ax.add_patch(circle)
+                ax.text(x+1.5, y+1.5, f'{freq:.2f}', ha='center', va='center', color='black', fontsize=8)
+            
+        
+        cbar2 = plt.colorbar(sm2, ax=ax, orientation='vertical', pad=0.1, location='left')
+        cbar2.set_label('Ace zone [%]')
+        cbar2.ax.tick_params(labelsize=8)
+        
 
 
 
 
+
+
+        # Centri approssimativi delle zone di attacco (lato inferiore)
+        zone_centers_att = {
+            1: (0+1.5,court_length+1-0.5), 
+            5: (2*court_width/3+1.5,court_length+1-0.5), 
+            6: (court_width/3+1.5,court_length+1-0.5)
+        }
+
+        # Centri approssimativi delle zone di difesa (lato superiore)
+        zone_centers_def = {
+            1: (2*court_width/3+1.5,0+1.5), 
+            2: (2*court_width/3+1.5,court_length/3+1.5),
+            3: (court_width/3+1.5,court_length/3+1.5),
+            4: (0+1.5,court_length/3+1.5), 
+            5: (0+1.5,0+1.5), 
+            6: (court_width/3+1.5,0+1.5),
+            8: (2*court_width/3+1.5,court_length/6+1.5),
+            9: (0+1.5,court_length/6+1.5),
+            10:(court_width/3+1.5,court_length/6+1.5)
+        }
+
+        # Disegna le frecce
+        max_freq_transizione = transizioni_frequenze.max().max() if not transizioni_frequenze.empty else 0.01 # Evita la divisione per zero
+
+        for att_zone, row in transizioni_frequenze.iterrows():
+            if att_zone in zone_centers_att:
+                x_start, y_start = zone_centers_att[att_zone]
+                for def_zone, freq in row.items():
+                    if def_zone in zone_centers_def and freq > soglia_freq:
+                        x_end, y_end = zone_centers_def[def_zone]
+                        larghezza = 5* (freq / max_freq_transizione)  # Larghezza base scalata
+                        scala_punta = 30 * (freq / max_freq_transizione)       # Scala della punta scalata
+
+                        arrow = FancyArrowPatch(
+                            (x_start, y_start), (x_end, y_end),
+                            arrowstyle="-|>",
+                            mutation_aspect=0.8,
+                            mutation_scale=scala_punta,
+                            connectionstyle="Arc3, rad=0.1",
+                            fc='black', ec='orange', alpha=0.5,
+                            lw=larghezza
+                        )
+                        ax.add_patch(arrow)
+        st.pyplot(fig) 
 
 
 
@@ -923,6 +1010,9 @@ st.session_state.fundamental_type = st.segmented_control("Choose the type of fun
 focus = all_match1[all_match1['player'] == st.session_state.player]
 
 #points/errors/both and fundamental
+
+############################################################################################
+
 if st.session_state.fundamental_type == "attack":
 
     focus_att = focus[focus['attack_zone'].notna()]
@@ -1002,7 +1092,8 @@ if st.session_state.fundamental_type == "attack":
         # Esegui la funzione per visualizzare il grafico
         plot_volleyball_attack_frequency(frequenza_attacchi,frequenza_difese,frequenza_transizioni, st.session_state.player, soglia_freq=min_frequenza_threshold)
 
-   
+#######################################################################################
+
 if st.session_state.fundamental_type == "serve":
 
     focus_serve = focus[focus['serve_zone'].notna()]
@@ -1084,9 +1175,9 @@ if st.session_state.fundamental_type == "serve":
 
         # Esegui la funzione per visualizzare il grafico
         plot_volleyball_serve_frequency(frequenza_servizi,frequenza_ace,frequenza_transizioni, st.session_state.player, soglia_freq=min_frequenza_threshold)
-
     
 ######################################################################################
+
 if st.session_state.fundamental_type == "block":
 
     focus_block = focus[focus['block_zone'].notna()]
@@ -1125,7 +1216,6 @@ if st.session_state.fundamental_type == "block":
         # Esegui la funzione per visualizzare il grafico
         plot_volleyball_block_frequency(frequenza_blocchi, st.session_state.player)
 
-
 ###########################################################################################
 
 if st.session_state.fundamental_type == "defense":
@@ -1136,7 +1226,7 @@ if st.session_state.fundamental_type == "defense":
     
     if st.session_state.info_type == "errors":
         focus_defense = focus_defense[(focus_defense['score'] == 'L') & (focus_defense['point_type'] == 'opp point') & (focus_defense['attack_zone'].notna())]
-        st.dataframe(focus_defense)
+
         defense = pd.DataFrame({
             'start_def' : focus_defense['attack_zone'].str.extract(r'att_(\d+)')[0].dropna().astype(int),
             'end_def' : focus_defense['defense_zone'].str.extract(r'def_(\d+)')[0].dropna().astype(int)
@@ -1162,7 +1252,6 @@ if st.session_state.fundamental_type == "defense":
         # Esegui la funzione per visualizzare il grafico
         plot_volleyball_defense_frequency(frequenza_attacchi,frequenza_difese,frequenza_transizioni, st.session_state.player, soglia_freq=min_frequenza_threshold)
 
-
 ###################################################################################################ààà
     
 if st.session_state.fundamental_type == "receive":
@@ -1173,7 +1262,33 @@ if st.session_state.fundamental_type == "receive":
     
     if st.session_state.info_type == "errors":
         focus_receive = focus_receive[(focus_receive['score'] == 'L') & (focus_receive['point_type'] == 'opp point') & (focus_receive['serve_zone'].notna())]
-        st.dataframe(focus_receive)
+        
 
+        receive = pd.DataFrame({
+            'start_rec' : focus_receive['serve_zone'].str.extract(r'serve_(\d+)')[0].dropna().astype(int),
+            'end_rec' : focus_receive['defense_zone'].str.extract(r'def_(\d+)')[0].dropna().astype(int)
+
+        })
+        receive = receive.reset_index(drop=True)
+        
+        #crea vettore con frequenza zone di servizio
+        frequenza_servizi = receive['start_rec'].value_counts(normalize=True).sort_index().reindex(range(1, 7), fill_value=0)
+        frequenza_ace = receive['end_rec'].value_counts(normalize=True).sort_index().reindex(range(1, 11), fill_value=0)
+        frequenza_transizioni = pd.crosstab(receive['start_rec'], receive['end_rec'], normalize=True)
+
+
+        min_frequenza_threshold = st.slider(
+            "Soglia minima frequenza transizione:",
+            min_value=0.0,
+            max_value=frequenza_transizioni.max().max() if not frequenza_transizioni.empty else 0.1,
+            value=0.01,  # Valore predefinito
+            step=0.001,
+            format="%.3f"
+        )
+
+        # Esegui la funzione per visualizzare il grafico
+        plot_volleyball_receive_frequency(frequenza_servizi,frequenza_ace,frequenza_transizioni, st.session_state.player, soglia_freq=min_frequenza_threshold)
+
+###########################################################################################
 
 
