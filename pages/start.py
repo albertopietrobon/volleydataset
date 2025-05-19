d1f7a30db30ac2ddd22562438e4fa4fce7981dc8
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-05-19 17:05:26 +0000

    c37

--- pages/start.py
+++ pages/start.py
@@ -1,11 +1,46 @@
 import streamlit as st
 import pandas as pd
-
+from PIL import Image
+import pathlib
 # PRIMA PAGINA, SCELTA TRA NEW GAME, GAME HISTORY E PLAYER STATS
 
-new_game = st.button("NEW GAME")
-game_history = st.button("GAME HISTORY")
-player_stats = st.button("PLAYER STATS")
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+css_path = pathlib.Path("assets/home_page.css")
+load_css(css_path)
+
+logo = Image.open("logo2.jpg")
+st.image(logo,use_container_width=True)
+
+
+
+bot1,im1 = st.columns([0.7,0.3])
+with bot1:
+    new_game = st.button("**NEW GAME**\n\nKeep track of the main actions of your players during a match with a highly interactive tool for reporting.\n\n Use the gathered data to look at the player and team statistics.",key="new_game",use_container_width=True)
+with im1:
+    report = Image.open("report.jpeg")
+    report = report.resize((report.width, 1000)) 
+    st.image(report, use_container_width=True)
+
+
+im2,bot2 = st.columns([0.3,0.7])
+with im2:
+    player = Image.open("player.jpg")
+    player = player.resize((player.width, 1050)) 
+    st.image(player,use_container_width=True)
+with bot2:
+    player_stats = st.button("**PLAYER STATS**\n\nLook at the statistics of your players focusing on the main fundamentals to prepare your future matches with deeper insights.",key="player_st",use_container_width=True)
+bot3,im3 = st.columns([0.7,0.3])
+with bot3:   
+    game_history = st.button("**TEAM STATS**\n\nLook at the statistics of your team focusing on the points and errors occured during previous games.",key="team_st",use_container_width=True)
+with im3:
+    team = Image.open("team.jpg")
+    team = team.resize((team.width, 1050)) 
+    st.image(team,use_container_width=True)
+
+
 
 if new_game:
     
